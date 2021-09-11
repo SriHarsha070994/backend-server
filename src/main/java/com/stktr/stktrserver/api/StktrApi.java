0fc66f72c9595e190c60313be6079cebcfe3259e
Harsha <sriharsha.jayanthi@initechglobal.com>
2021-09-11 07:45:12 +0530

    Api to return a string along with the value passed in the environment variable!

---
+++ src/main/java/com/stktr/stktrserver/api/StktrApi.java
@@ -0,0 +1,24 @@
+package com.stktr.stktrserver.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stktr.stktrserver.util.Constants;
+
+@RestController
+public class StktrApi {
+	@RequestMapping("/")
+
+	public ResponseEntity<String> getBook() {
+		if (System.getenv(Constants.ENV_VARAIBLE) == null)
+			return new ResponseEntity<String>("Environment Varaible: " + Constants.ENV_VARAIBLE + " is missing",
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		if (System.getenv(Constants.ENV_VARAIBLE).isBlank())
+			return new ResponseEntity<String>(
+					"Value for the Environment Varaible: " + Constants.ENV_VARAIBLE + " is not set",
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<String>("Hello " + System.getenv(Constants.ENV_VARAIBLE), HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/stktr/stktrserver/util/Constants.java
@@ -0,0 +1,7 @@
+package com.stktr.stktrserver.util;
+
+public class Constants {
+
+	public static final String ENV_VARAIBLE = "Name";
+	
+}
