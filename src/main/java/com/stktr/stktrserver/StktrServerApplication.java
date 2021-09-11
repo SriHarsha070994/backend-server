e12720675aa75a39c5ada7e01125821fa001603d
Harsha <sriharsha.jayanthi@initechglobal.com>
2021-09-11 07:35:28 +0530

    Initial commit!

---
+++ src/main/java/com/stktr/stktrserver/StktrServerApplication.java
@@ -0,0 +1,13 @@
+package com.stktr.stktrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StktrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StktrServerApplication.class, args);
+	}
+
+}
